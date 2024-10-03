e6044c892d552ad150d4528cccc9c6ddb56bfacf
pingstyy <168405537+pingstyy@users.noreply.github.com>
2024-10-04 03:22:40 +0530

    Create bash.bash

---
+++ nvim/bash.bash
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install neovim fuse wget
+
+wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin/nvim
+
+
+nvim --version
